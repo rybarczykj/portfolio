94bffd327bb320969db639c868f026d254f33b71
rybarczykj <jtrybarczyk@gmail.com>
2023-09-23 14:05:24 -0400

    initial skeleton and styling!

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,11 +1,33 @@
-import React from 'react';
 import './homepage.less'
 
 export const Homepage = () => {
     return (
-        <div className='introduction'>
-            <h1 className="text">JACK RYBARCZYK is a mf uhhh frontend engineer + graphic_designer. based in NYC. currently @ datadog and doesn’t understand negative space</h1>
+        <div className='portfolio'>
+            <div className='flex'>
+                <div className='introduction'>
+                    <span className="text">JACK RYBARCZYK is a mf uhhh frontend engineer + graphic_designer. based in NYC. currently @ datadog and doesn’t understand <span className='highlighted'>negative space
+                    </span>.
+                    </span>
+                </div>
+
+                <div className='projects'>
+                    <ul>
+                        <li>user interfaces</li>
+                        <li>tools for designers</li>
+                        <li>video processing</li>
+                        <li>interactive sculpture</li>
+
+
+                    </ul>
+                </div>
+
+
+
+
+            </div >
         </div>
+
+
     );
 }
 
