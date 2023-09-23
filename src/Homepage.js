d10a46c3d5352a1a6a058e0b9e77f1e9b38d23e0
rybarczykj <jtrybarczyk@gmail.com>
2023-09-23 12:48:17 -0400

    add hompage

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.less'
+
+export const Homepage = () => {
+    return (
+        <div className='introduction'>
+            <h1 className="text">JACK RYBARCZYK is a mf uhhh frontend engineer + graphic_designer. based in NYC. currently @ datadog and doesn’t understand negative space</h1>
+        </div>
+    );
+}
+
+
+export default Homepage;
