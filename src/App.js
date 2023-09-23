2cf6b709c12de1ae58c3f4e8e5d10999fc404ca7
rybarczykj <jtrybarczyk@gmail.com>
2023-09-23 12:26:16 -0400

    removing stuff i'm not gonna use

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      helllo
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
+          HELLELELELELELs
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
